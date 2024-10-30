aaca2af6071cfd6d385b684adc60cf1aa0e90dca
Subhansh malviya <167747484+subh-775@users.noreply.github.com>
2024-10-30 19:12:33 +0530

    Add files via upload
    Check.py file is a script file to check whether your credentials authentication is successful or not. You can prior run this file before running main file to know your credentials state

---
+++ check.py
@@ -0,0 +1,15 @@
+#A small script to test your credentials outside the main project to check whether authentication is successful.
+import smtplib
+
+SENDER_EMAIL = "Enter your email"
+APP_PASSWORD = "Enter your app password "
+
+try:
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(SENDER_EMAIL, APP_PASSWORD)
+        print("Authentication successful!")
+except smtplib.SMTPAuthenticationError:
+    print("Error: Authentication failed. Check your email and app password.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
